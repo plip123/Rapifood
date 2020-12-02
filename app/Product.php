6c8e2f526b6d86345ca15f77ab39e17885bda709
Fernando Chavez <fc9chavez@gmail.com>
2020-12-02 19:37:29 -0400

    Add slug to product

--- app/Product.php
+++ app/Product.php
@@ -4,15 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     protected $table = "product";
     public $primaryKey = "id";
     public $timestamps = true;
-    protected $appends = ['image_url'];
+    protected $appends = ['image_url', 'link'];
 
     public function getImageUrlAttribute(){
         return Storage::url($this->image);
+    }
+
+    public function getLinkAttribute(){
+        return "/combo" + Str::slug($this->name) + "/" + $this->id;
     }
 }
