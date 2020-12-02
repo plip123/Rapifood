8fe8650d54f42a771e865f65af6f957f824b63f2
Fernando Chavez <fc9chavez@gmail.com>
2020-12-02 19:46:53 -0400

    fix bugs

--- app/Product.php
+++ app/Product.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use App\Store;
 use Illuminate\Support\Str;
 
 class Product extends Model
@@ -11,13 +12,17 @@ class Product extends Model
     protected $table = "product";
     public $primaryKey = "id";
     public $timestamps = true;
-    protected $appends = ['image_url', 'link'];
+    protected $appends = ['image_url', 'restaurant_name', 'link'];
 
     public function getImageUrlAttribute(){
         return Storage::url($this->image);
     }
 
+    public function getRestaurantNameAttribute(){
+        return Store::find($this->storeID)->name;
+    }
+
     public function getLinkAttribute(){
-        return "/combo" + Str::slug($this->name) + "/" + $this->id;
+        return "/combo" . "/" . Str::slug($this->name) . "/" . $this->id;
     }
 }
