f5460ddea4de7151e6a9bb5346128367577e8d5e
Fernando Chavez <fc9chavez@gmail.com>
2020-12-03 07:37:28 -0400

    Product routes fix

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,12 @@ Route::post('signup','Auth\RegisterController@store');
 Route::post('login','Auth\LoginController@login');
 
 Route::post('productFilter', 'ProductController@productFilter');
-Route::resource('product', 'ProductController');
+Route::get('product', 'ProductController@index');
+Route::get('product/{id}', 'ProductController@show');
 
 
 Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('product', 'ProductController@store');
     Route::resource('payment','PaymentController');
     Route::post('pay','PaymentGatewayController@index');
     Route::resource('store', 'StoreController');
